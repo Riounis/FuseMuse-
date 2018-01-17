02afcac365fcbb9f8cbc7a2d4cc3895af5dab5a4
sjrappl <sjrappl@ncsu.edu>
2018-01-17 16:01:33 -0500

    Test Test

---
+++ tests/testtest.cpp
@@ -0,0 +1,11 @@
+#include "note.h"
+#include "gtest/gtest.h"
+
+TEST(TestTest, TestTest) {
+	
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}

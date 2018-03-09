48f58f3fcf0b3c3a5d3f65e1b59b12a13acc2714
Sam Rappl <samrappl50@gmail.com>
2018-03-08 22:05:39 -0500

    added user functionality to key

--- src/libfm/composition_metrics.h
+++ src/libfm/composition_metrics.h
@@ -12,7 +12,7 @@
 #include "constants.h"
 
 struct TimeSignature {
-    TimeSignature(int num = 4, int denom = 4) : num(num), denom(denom) {
+    TimeSignature(char num = 4, char denom = 4) : num(num), denom(denom) {
 		// If the denominator is not a power of 2
 		if ((denom & (denom - 1)) != 0) {
 			// Eventually throw error; for now, set to 4
@@ -28,6 +28,9 @@ public:
     Key(int tonic = c3, std::vector<char> intervals = major_intervals) :
             tonic(tonic), intervals(intervals) {
         char pitch = tonic;
+		if (pitch >= 102) {
+			pitch = pitch % 12 + 36;
+		}
         scale.push_back(pitch);
         for (std::vector<char>::size_type i = 0; i < intervals.size(); i++) {
             pitch += intervals[i];
@@ -36,8 +39,183 @@ public:
     }
 	std::vector<char> getScale() { return scale; }
 	std::vector<char> getIntervals() { return intervals; }
+	int getKeySignature() {
+		if (intervals != major_intervals && intervals != minor_intervals) {
+			return 0;
+		}
+		else if (intervals == major_intervals) {
+			int currentValue = 0;
+			for (int i = 0; i < 12; i++) {
+				if (tonic % 12 == i) {
+					return currentValue;
+				}
+				if (currentValue >= 0) {
+					currentValue -= 5;
+				}
+				else {
+					currentValue += 7;
+				}
+			}
+		}
+		else {
+			int currentValue = -3;
+			for (int i = 0; i < 12; i++) {
+				if (tonic % 12 == i) {
+					return currentValue;
+				}
+				if (currentValue >= 0) {
+					currentValue -= 5;
+				}
+				else {
+					currentValue += 7;
+				}
+			}
+		}
+	}
+	int getKeyQuality() {
+		if (tonic + 4 == scale[2]) {
+			return 0;
+		}
+		else if (tonic + 3 == scale[2]) {
+			return 1;
+		}
+		else {
+			return 2;
+		}
+	}
+	bool isInScale(char pitch) {
+		char pmod = pitch % 12;
+		for (int i = 0; i < scale.size(); i++) {
+			if (pmod == scale[i] % 12) {
+				return true;
+			}
+		}
+		return false;
+	}
+	int positionInScale(char pitch) {
+		char pmod = pitch % 12;
+		for (int i = 0; i < scale.size(); i++) {
+			if (pmod == scale[i] % 12) {
+				return i + 1;
+			}
+		}
+		return -1;
+	}
+	int nextPitchInScale(char pitch) {
+		char numOctaves = pitch / 12;
+		char pmod = pitch % 12;
+		int indexOfNext = -1;
+		for (int i = 0; i < scale.size(); i++) {
+			if (pmod == scale[i] % 12) {
+				indexOfNext = i+1;
+			}
+		}
+		if (indexOfNext == -1) {
+			for (int i = 0; i < scale.size() - 1; i++) {
+				if (pmod > scale[i] % 12 && pmod < scale[i+1] % 12) {
+					indexOfNext = i + 1;
+				}
+			}
+		}
+		return scale[indexOfNext] % 12 + numOctaves * 12;
+	}
+	char getNthPitch(int n) {
+		if (n < scale.size() && n >= 0) {
+			return scale[n-1];
+		}
+		else {
+			return -1;
+		}
+	}
 	char getTonic() { return tonic; }
-	//char getKeySignature
+	char get2nd() {
+		if (scale.size() >= 2) {
+			return scale[1];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get3rd() {
+		if (scale.size() >= 3) {
+			return scale[2];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get4th() {
+		if (scale.size() >= 4) {
+			return scale[3];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get5th() {
+		if (scale.size() >= 5) {
+			return scale[4];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get6th() {
+		if (scale.size() >= 6) {
+			return scale[5];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get7th() {
+		if (scale.size() >= 7) {
+			return scale[6];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get8th() {
+		if (scale.size() >= 8) {
+			return scale[7];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get9th() {
+		if (scale.size() >= 9) {
+			return scale[8];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get10th() {
+		if (scale.size() >= 10) {
+			return scale[9];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get11th() {
+		if (scale.size() >= 11) {
+			return scale[10];
+		}
+		else {
+			return -1;
+		}
+	}
+	char get12th() {
+		if (scale.size() >= 12) {
+			return scale[11];
+		}
+		else {
+			return -1;
+		}
+	}
 private:
     char tonic;
     std::vector<char> intervals;
--- tests/compositionmetricstest.cpp
+++ tests/compositionmetricstest.cpp
@@ -9,13 +9,13 @@
 #include "gtest/gtest.h"
 #include <vector>
 
-TEST(compositionMetricsTest, timeSigDefaultConstructorTest) {
+TEST(timeSigTest, timeSigDefaultConstructorTest) {
 	TimeSignature timeSig;
 	ASSERT_EQ(4, timeSig.num);
 	ASSERT_EQ(4, timeSig.denom);
 }
 
-TEST(compositionMetricsTest, timeSigConstructorTest) {
+TEST(timeSigTest, timeSigConstructorTest) {
 	TimeSignature timeSig(6,8);
 	ASSERT_EQ(6, timeSig.num);
 	ASSERT_EQ(8, timeSig.denom);
@@ -40,7 +40,7 @@ TEST(compositionMetricsTest, keyDefaultConstructorTest) {
 	ASSERT_EQ(major_intervals, key.getIntervals());
 }
 
-TEST(compositionMetricsTest, keyConstructorTest) {
+TEST(keyTest, keyConstructorTest) {
 	Key key(ds3, minor_intervals);
 	std::vector<char> scale;
 	scale.push_back(ds3);
@@ -54,6 +54,132 @@ TEST(compositionMetricsTest, keyConstructorTest) {
 	ASSERT_EQ(ds3, key.getTonic());
 	ASSERT_EQ(scale, key.getScale());
 	ASSERT_EQ(minor_intervals, key.getIntervals());
+}
+
+TEST(keyTest, keySignatureTest) {
+	Key key(g4, major_intervals);
+	switch (key.getTonic() % 12) {
+		case 0:
+			ASSERT_EQ(0, key.getKeySignature());
+			break;
+		case 1:
+			ASSERT_EQ(-5, key.getKeySignature());
+			break;
+		case 2:
+			ASSERT_EQ(2, key.getKeySignature());
+			break;
+		case 3:
+			ASSERT_EQ(-3, key.getKeySignature());
+			break;
+		case 4:
+			ASSERT_EQ(4, key.getKeySignature());
+			break;
+		case 5:
+			ASSERT_EQ(-1, key.getKeySignature());
+			break;
+		case 6:
+			ASSERT_EQ(6, key.getKeySignature());
+			break;
+		case 7:
+			ASSERT_EQ(1, key.getKeySignature());
+			break;
+		case 8:
+			ASSERT_EQ(-4, key.getKeySignature());
+			break;
+		case 9:
+			ASSERT_EQ(3, key.getKeySignature());
+			break;
+		case 10:
+			ASSERT_EQ(-2, key.getKeySignature());
+			break;
+		case 11:
+			ASSERT_EQ(3, key.getKeySignature());
+			break;
+	}
+	Key key2(cs3, minor_intervals);
+	switch (key2.getTonic() % 12) {
+		case 0:
+			ASSERT_EQ(-3, key2.getKeySignature());
+			break;
+		case 1:
+			ASSERT_EQ(4, key2.getKeySignature());
+			break;
+		case 2:
+			ASSERT_EQ(-1, key2.getKeySignature());
+			break;
+		case 3:
+			ASSERT_EQ(6, key2.getKeySignature());
+			break;
+		case 4:
+			ASSERT_EQ(1, key2.getKeySignature());
+			break;
+		case 5:
+			ASSERT_EQ(-4, key2.getKeySignature());
+			break;
+		case 6:
+			ASSERT_EQ(3, key2.getKeySignature());
+			break;
+		case 7:
+			ASSERT_EQ(-2, key2.getKeySignature());
+			break;
+		case 8:
+			ASSERT_EQ(5, key2.getKeySignature());
+			break;
+		case 9:
+			ASSERT_EQ(0, key2.getKeySignature());
+			break;
+		case 10:
+			ASSERT_EQ(-5, key2.getKeySignature());
+			break;
+		case 11:
+			ASSERT_EQ(2, key2.getKeySignature());
+			break;
+	}
+	Key key3(gf3, mixolydian_intervals);
+	ASSERT_EQ(0, key3.getKeySignature());
+}
+
+TEST(keyTest, keyQualityTest) {
+	Key key(gs2, minor_intervals);
+	ASSERT_EQ(1, key.getKeyQuality());
+	Key key2(c3, major_intervals);
+	ASSERT_EQ(0, key2.getKeyQuality());
+	Key key3(d3, dorian_intervals);
+	ASSERT_EQ(1, key3.getKeyQuality());
+}
+
+TEST(keyTest, inScaleTest) {
+	Key key(c3, major_intervals);
+	ASSERT_EQ(false, key.isInScale(gs3));
+	ASSERT_EQ(true, key.isInScale(b3));
+	ASSERT_EQ(true, key.isInScale(d5));
+}
+
+TEST(keyTest, positionInScaleTest) {
+	Key key(c3, lydian_intervals);
+	ASSERT_EQ(4, key.positionInScale(fs3));
+	ASSERT_EQ(2, key.positionInScale(d6));
+	ASSERT_EQ(1, key.positionInScale(c0));
+	ASSERT_EQ(-1, key.positionInScale(cs4));
+}
+
+TEST(keyTest, nextPitchInScaleTest) {
+	Key key(c3, lydian_intervals);
+	ASSERT_EQ(fs3, key.nextPitchInScale(e3));
+	ASSERT_EQ(fs3, key.nextPitchInScale(f3));
+	ASSERT_EQ(g4, key.nextPitchInScale(fs4));
+}
+
+TEST(keyTest, nthPitchTest) {
+	Key key(c3, lydian_intervals);
+	ASSERT_EQ(c3, key.getTonic());
+	ASSERT_EQ(d3, key.get2nd());
+	ASSERT_EQ(e3, key.get3rd());
+	ASSERT_EQ(fs3, key.get4th());
+	ASSERT_EQ(g3, key.get5th());
+	ASSERT_EQ(a3, key.get6th());
+	ASSERT_EQ(b3, key.get7th());
+	ASSERT_EQ(fs3, key.getNthPitch(4));
 }
 
 TEST(compositionMetricsTest, compositionMetricsDefaultConstructorTest) {
@@ -79,3 +205,4 @@ TEST(compositionMetricsTest, compositionMetricsDefaultConstructorTest) {
 	// Test the measure position
 	ASSERT_EQ(0, comp.measure);
 }
+
